ae532485338afd90f736727cd4b9f15901575dce
Daniel Nathanson <dana@odoo.com>
2023-06-29 14:03:58 -0400

    finished vertosa customization request

---
+++ vertosa_active_fields/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Vertosa Active Fields",
+    
+    "summary": "Adding 'Main Active Potency' and 'Main Active Quantity' fields",
+    
+    "description": """
+Vertosa Active Fields
+=====================
+This Module is used to add the constrained field 'Main Active Potency' (type:: integer) and the computed field 'Main Active Quantity' (type:: float).
+    """,
+    
+    "version": "1.0.0",
+    
+    "category": "Vertosa/Customizations",
+    
+    "license": "OPL-1",
+    
+    "depends": ["stock"],
+    
+    "author": "vertosa",
+    
+    "website": "www.vertosa.com",
+}
